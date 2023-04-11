d55d81ea2b8302e8988c72bfff777c4b23c40ed8
Jensone <hello@jensone.com>
2023-04-11 18:02:20 +0200

    🔨 Add authentication

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -13,26 +13,15 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractDashboardController
 {
-    #[Route('/admin', name: 'admin', methods: ['GET', 'POST'])]
+    #[Route('/patate92', name: 'admin', methods: ['GET', 'POST'])]
     public function index(): Response
     {
         return $this->render('admin/index.html.twig');
+    }
 
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
-        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
-
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirect('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
-        // return $this->render('some/path/my-dashboard.html.twig');
+    #[Route('/admin')]
+    public function redirectToHome() {
+        return $this->redirectToRoute('app_home');
     }
 
     public function configureDashboard(): Dashboard
@@ -48,6 +37,5 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToCrud('Projects', 'fas fa-cubes', Projects::class);
         yield MenuItem::linkToCrud('Experiences', 'fas fa-cubes', Experience::class);
         yield MenuItem::linkToCrud('Education', 'fas fa-cubes', Education::class);
-
     }
 }
